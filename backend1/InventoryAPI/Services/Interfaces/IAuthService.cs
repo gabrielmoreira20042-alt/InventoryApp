79e0ff9dbb3b8cf9a167e4b348c97b5a1f609f53
1221318 <1221318@isep.ipp.pt>
2026-03-11 01:22:00 +0000

    changed backend1 to backend

--- backend1/InventoryAPI/Services/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using InventoryAPI.DTOs;
-
-namespace InventoryAPI.Services.Interfaces;
-
-public interface IAuthService
-{
-    Task<AuthResponseDto?> RegisterAsync(RegisterDto dto);
-    Task<AuthResponseDto?> LoginAsync(LoginDto dto);
-}
--- backend1/InventoryAPI/Services/Interfaces/ICategoryService.cs
+++
@@ -1,9 +0,0 @@
-using InventoryAPI.DTOs;
-
-namespace InventoryAPI.Services.Interfaces;
-
-public interface ICategoryService
-{
-    Task<List<CategoryResponseDto>> GetAllAsync();
-    Task<CategoryResponseDto> CreateAsync(CategoryCreateDto dto);
-}
